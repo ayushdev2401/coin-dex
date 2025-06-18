1052d1e3952271d2bc4a9970fb7b892ffbc03816
ayushdev2401 <career.ayushbh@gmail.com>
2025-06-19 01:50:17 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import SendETH from './pages/SendETH';
-import NFTViewer from './pages/NFTViewer';
-
-function Home() {
-  return <div className="p-4">Welcome to Coin DEX! Choose an option from the menu.</div>;
-}
-
-function Buy() {
-  return <div className="p-4">Buy Ethereum options coming soon.</div>;
-}
-
-function Compare() {
-  return <div className="p-4">Compare exchanges and rates here.</div>;
-}
-
-function Learn() {
-  return <div className="p-4">Educational resources about Ethereum.</div>;
-}
+import React from "react";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/compare" element={<Compare />} />
-        <Route path="/learn" element={<Learn />} />
-        <Route path="/send" element={<SendETH />} />
-        <Route path="/nfts" element={<NFTViewer />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Welcome to Coin DEX</h1>
+      <p>MetaMask integration, ETH transfer, NFT viewer enabled.</p>
+    </div>
   );
 }
 
