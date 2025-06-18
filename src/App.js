e18af351c60f198848d1bd01138770e84240f229
ayushdev2401 <career.ayushbh@gmail.com>
2025-06-19 02:12:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Coin DEX</h1>
+      <p>MetaMask integration, ETH transfer, NFT viewer enabled.</p>
+    </div>
+  );
+}
+
+export default App;
